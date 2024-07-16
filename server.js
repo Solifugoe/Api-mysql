309ee16d8a7a3795c20a00df90a59cf863d79372
Armando Siqueiros <aikdvs@gmail.com>
2024-07-16 11:33:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,94 +1,35 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const pool = require("./conexion");
+const datos_usuarios = require("./routes/datos_usuarios");
+const emo_men = require("./routes/historial_emociones_mensuales");
+const emo_nega = require("./routes/emo_negativas");
+const emo_pos = require("./routes/emo_positivas");
+const emo_neu = require("./routes/emo_neutras");
+const usuario = require("./routes/usuario");
+const login = require("./routes/login");
 
 const app = express();
-const port = 5000;
-
-app.use(cors());
 app.use(bodyParser.json());
 
-// Configura la conexión a la base de datos
-const db = mysql.createConnection({
-  host: 'oaxacapower.org',
-  user: 'u744130986_theraglow',
-  password: 'KaizoIntegradora2024',
-  database: 'u744130986_theraglow'
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
 });
 
-db.connect(err => {
-  if (err) {
-    throw err;
-  }
-  console.log('Connected to MySQL database');
-});
-
-// Define rutas API
-app.get('/api/usuarios', (req, res) => {
-  const sql = 'SELECT * FROM datos_usuario';
-  db.query(sql, (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.json(results);
-  });
-});
-
-app.post('/api/usuarios', (req, res) => {
-  const newItem = req.body;
-  const sql = 'INSERT INTO datos_usuario SET ?';
-  db.query(sql, newItem, (err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.status(201).send(result);
-  });
-});
-
-// Nueva ruta para datos de emociones
-app.get('/api/emociones', (req, res) => {
-  const sqlNegativas = 'SELECT * FROM emociones_negativas';
-  const sqlNeutras = 'SELECT * FROM emociones_neutras';
-  const sqlPositivas = 'SELECT * FROM emociones_positivas';
-
-  const emociones = {};
-
-  db.query(sqlNegativas, (err, resultadosNegativos) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    emociones.negativas = resultadosNegativos;
-
-    db.query(sqlNeutras, (err, resultadosNeutros) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      emociones.neutras = resultadosNeutros;
-
-      db.query(sqlPositivas, (err, resultadosPositivos) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        emociones.positivas = resultadosPositivos;
-
-        res.json(emociones);
-      });
-    });
-  });
-});
-
-// Nueva ruta para datos de historial de emociones mensual
-app.get('/api/historial-emociones-mensual', (req, res) => {
-  const sql = 'SELECT * FROM historial_emociones_mensual';
-  db.query(sql, (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.json(results);
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Rutas de la API
+app.use("/datos_usuario", datos_usuarios);
+app.use("/historial_emociones_mensuales", emo_men);
+app.use("/emociones_negativas", emo_nega);
+app.use("/emociones_positivas", emo_pos);
+app.use("/emociones_neutras", emo_neu);
+app.use("/usuario", usuario);
+app.use("/login", login);
+// Puerto en el que escucha el servidor
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
